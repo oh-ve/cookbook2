0502f53e94e56603aa867a446353aad151c06607
oh-ve <maricic.ve@gmail.com>
2022-11-22 16:21:50 +0100

    structure

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,10 +1,7 @@
 import "./App.css";
-
 import { useEffect, useState } from "react";
 import { client } from "./client";
 
-function App() {
-  return <div className="App"></div>;
+export default function Categories() {
+  return <div></div>;
 }
-
-export default App;
