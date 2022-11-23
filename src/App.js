2ecb1a195aff743ea4d1a48f2256683f48b380d6
oh-ve <maricic.ve@gmail.com>
2022-11-23 12:00:19 +0100

    first steps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { client } from "./client";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    client
+      .getEntries()
+      .then((res) => {
+        setRecipes(res.items);
+
+        console.log(res.items);
+      })
+
+      .catch((e) => console.log(e));
+  }, []);
+
+  return (
+    <div className="App">
+      <Navbar />
+      {/* <Routes>
+        <Route path="/" element=></Route>
+        <Route></Route>
+      </Routes> */}
+      <h1>Hello</h1>
+      {recipes.map((recipe) => (
+        <div>
+          <h1>{recipe.fields.title}</h1>
+          <img
+            src={recipe.fields.image.fields.file.url}
+            width="200px"
+            height="200px"
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,17 @@
 import React from "react";
+//import { Routes, Route } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <h1>Navbar</h1>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/breakfast">Breakfast</NavLink>
+        <NavLink to="/lunch">Lunch</NavLink>
+        <NavLink to="/dinner">Dinner</NavLink>
+      </nav>
+    </div>
+  );
 }
