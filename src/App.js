6264d003e3f1da9cd0e7fa92375d1cac9ddb4f5a
Jack Snape <jacksnape89@gmail.com>
2022-11-23 12:00:35 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 import { useEffect, useState } from "react";
 import { client } from "./client";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,16 @@
-import React from "react";
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Bad British Food</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/recipes">Breakfast</a>
+        <a href="/recipes">Lunch</a>
+        <a href="/recipes">Dinner</a>
+        <a href="/recipes">Desert</a>
+      </div>
+    </nav>
+  );
+};
 
-export default function Navbar() {
-  return <div>Navbar</div>;
-}
+export default Navbar;
