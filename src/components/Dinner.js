6794108c5e5279330a0ff8abff17271512ddc9d7
Payal Thummar <payalthummar1994@gmail.com>
2022-11-24 10:25:40 +0100

    dinner part

--- src/components/Dinner.js
+++ src/components/Dinner.js
@@ -1,6 +1,38 @@
 import "../App.css";
 import { useEffect, useState } from "react";
+import { marked } from "marked";
+
+import { Link } from "react-router-dom";
 
 export default function Dinner(props) {
-  return <div></div>;
+  console.log("props", props.recipes);
+  const items = props.recipes.filter((data) => {
+    return data.fields.category === "dinner";
+  });
+  console.log("items", items);
+
+  return (
+    <div className="App">
+      {/* <h1>Category: {props.recipes[0].fields.category}</h1>
+      <img src={props.recipes[0].fields.image.fields.file.url} />
+      <img src={props.recipes[1].fields.image.fields.file.url} /> */}
+
+      {items.map((recipe, key) => {
+        return (
+          <div>
+            {recipe.fields.id}
+            <Link to={`/dinner/${recipe.fields.id}`}>
+              <h1>{recipe.fields.title}</h1>
+            </Link>
+            {/* <h2>{recipe.items}</h2> */}
+            <img
+              src={recipe.fields.image.fields.file.url}
+              width="200px"
+              height="200px"
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
-export default function Recipes() {
-  return <div>Recipes</div>;
+export default function Recipes({ recipes }) {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const SingleRecipe = recipes.find(
+    (element) => element.fields.id === Number(id)
+  );
+  console.log("Hello recipes", SingleRecipe);
+  console.log(recipes);
+  // const SingleRecipe = recipes.find((t) => t.id === id);
+  // console.log("Hello", SingleRecipe);
+  return (
+    <div>
+      {SingleRecipe.fields.title}
+      {SingleRecipe.fields.ingredients}
+
+      <p>Hello</p>
+    </div>
+  );
 }
